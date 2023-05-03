44f848eeacaa130464cc245e33e4fee8b784f624
HasanMasud1039 <hasanmasud1039@gmail.com>
2023-05-04 01:50:30 +0600

    first commit

---
+++ src/shared/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <h6>foot</h6>
+        </div>
+    );
+};
+
+export default Footer;
